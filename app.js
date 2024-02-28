c29b213a95c16658fd833be502bccb34e8f83ca1
BoHyun-Choi-0320 <320fleur@gmail.com>
2024-02-28 13:59:26 +0900

    "01) 페이지 생성 및 연결"

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const app = express();
 const hostname = "127.0.0.1";
 const port = 3000;
 
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", (res, req) => {
-  req.send("Hello, World!");
+  req.sendFile(__dirname + "/public/view/index.html");
 });
 
 app.listen(port, () => {
